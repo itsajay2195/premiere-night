773e6d495de222db7dac25857b5e5496992ec607
Ajaykumar Rajasekaran <ajayrne@gmail.com>
2026-03-04 22:13:05 +0530

    created image util

---
+++ src/utils/imageUtils.ts
@@ -0,0 +1,9 @@
+import { IMAGE_BASE } from '../constants/apiConstants';
+
+type PosterSize = 'w185' | 'w342' | 'w500' | 'original';
+
+export const getPosterUrl = (path: string | null, size: PosterSize = 'w342') =>
+  path ? `${IMAGE_BASE}/${size}${path}` : null;
+
+export const getBackdropUrl = (path: string | null) =>
+  path ? `${IMAGE_BASE}/w1280${path}` : null;
