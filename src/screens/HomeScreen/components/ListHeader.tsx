e6feff35cfa2c7e31437605b8d95f62c39983454
Ajaykumar Rajasekaran <ajayrne@gmail.com>
2026-03-07 23:29:49 +0530

    created buildscrions and moved listheader component toa separate file

---
+++ src/screens/HomeScreen/components/ListHeader.tsx
@@ -0,0 +1,69 @@
+import { StyleSheet, View } from 'react-native';
+import React, { memo } from 'react';
+import { Genre } from '../../../api/types/movie';
+import { GenreFilter } from './GenreFilter';
+import { SearchBar } from '../../../components/Searchbar/Searchbar';
+import { Typography } from '../../../components/Typography/Typography';
+import { Colors, Spacing } from '../../../theme/theme';
+
+interface ListHeaderProps {
+  searchQuery: string;
+  genres: Genre[];
+  selectedGenre: number | null;
+  isSearching: boolean;
+  onChangeText: (text: string) => void;
+  onSelectGenre: (id: number | null) => void;
+}
+
+const ListHeader = ({
+  searchQuery,
+  genres,
+  selectedGenre,
+  isSearching,
+  onChangeText,
+  onSelectGenre,
+}: ListHeaderProps) => {
+  return (
+    <View>
+      <View style={styles.header}>
+        <Typography variant="heading">PREMIERE</Typography>
+        <Typography
+          variant="caption"
+          color={Colors.accent}
+          style={styles.logoSub}
+        >
+          NIGHT
+        </Typography>
+      </View>
+      <View style={styles.searchContainer}>
+        <SearchBar value={searchQuery} onChangeText={onChangeText} />
+      </View>
+      {!isSearching && (
+        <View style={styles.genreContainer}>
+          <GenreFilter
+            genres={genres}
+            selected={selectedGenre}
+            onSelect={onSelectGenre}
+          />
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default memo(ListHeader);
+
+const styles = StyleSheet.create({
+  safe: { flex: 1, backgroundColor: Colors.background },
+  header: {
+    paddingHorizontal: Spacing.md,
+    paddingTop: Spacing.lg,
+    paddingBottom: Spacing.sm,
+  },
+  logoSub: { letterSpacing: 8, fontWeight: '700', marginTop: -4 },
+  searchContainer: {
+    paddingHorizontal: Spacing.md,
+    paddingVertical: Spacing.sm,
+  },
+  genreContainer: { marginBottom: Spacing.sm },
+});
